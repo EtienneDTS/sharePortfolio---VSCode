8ec84dd2efc3bcfce82bfe3491a457f34c5a245a
EtienneDTS <detoisienetienne@gmail.com>
2025-03-19 09:56:30 +0100

    amelioration de la partie action et action comp

--- src/main/java/tp04/metier/ActionComposee.java
+++ src/main/java/tp04/metier/ActionComposee.java
@@ -18,13 +18,9 @@ package tp04.metier;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- *
- * @author perussel
- */
 public class ActionComposee extends Action {
-    // attribut lien
-    public Map<ActionSimple, Float> mapPanier;
+
+    private Map<ActionSimple, Float> mapPanier;
 
     public ActionComposee(String libelle) {
         super(libelle);
@@ -32,19 +28,17 @@ public class ActionComposee extends Action {
     }
 
     public void enrgComposition(ActionSimple as, float pourcentage) {
-        this.mapPanier.put(as, pourcentage);
+        mapPanier.put(as, pourcentage);
     }
 
     @Override
     public float valeur(Jour j) {
-        float valeur;
-
-        valeur = 0;
-        for (ActionSimple as : this.mapPanier.keySet()) {
-            valeur = valeur + (as.valeur(j) * this.mapPanier.get(as));
+        float valeur = 0;
+        for (Map.Entry<ActionSimple, Float> entry : mapPanier.entrySet()) {
+            ActionSimple actionSimple = entry.getKey();
+            Float pourcentage = entry.getValue();
+            valeur += actionSimple.valeur(j) * pourcentage;
         }
-
         return valeur;
     }
-
 }
